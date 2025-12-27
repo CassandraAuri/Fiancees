471b28a7885543f86d1e8add931733f9d57e68e5
CassandraAuri <cassandramckenna456@gmail.com>
2025-12-27 16:02:52 -0400

    test

---
+++ Frontend/Home.py
@@ -0,0 +1,83 @@
+import glob 
+import pandas as pd
+import numpy as np
+from numpy.typing import NDArray
+from typing import Any
+import camelot
+import calendar
+import streamlit as st
+import datetime
+class csvlogic():
+    def dateformating(year, month):
+
+    def readcsv() -> None:
+        if "csvmasterfile" not in st.session_state:
+            st.session_state["csvmasterfile"] = pd.readcsv()#TODO get file when stupid backend dev gets it done (rolls eyes), im going to prettify now
+            pass
+    def userselecteddataframe() -> None:
+        
+        userdataframe=st.session_state["csvmasterfile"]
+
+        #Select Accounts
+        userdataframe=userdataframe["Account"].isin(st.session_state["Accounts"])
+        #Select Date
+
+        mask = (userdataframe['date'] > st.session_state[""]) & (df['date'] <= end_date)
+        userdataframe=userdataframe[]
+
+        st.session_state["UserDataframe"]
+
+class Dates_UserSelected():
+    
+    def monthslider() -> None:
+        months = list(calendar.month_name)[1:]  # January → December
+        if "Months" not in st.session_state:
+            st.session_state["Months"]=[months[0], months[-1]]
+        st.select_slider("Month Range",options=months, value=st.session_state["Months"], key="Months")
+        if st.session_state["Months"][0] == st.session_state["Months"][1]:
+            st.warning("Choose a Month range, not the same month")
+
+    def yearslider() -> None:
+        years = [2023,2024,2025,2026]
+        
+        if "Years" not in st.session_state:
+            st.session_state["Years"] = [2023, 2026]
+        
+        st.select_slider("Month Range",options=years, value=st.session_state["Years"], key="Years")
+
+        
+        if st.session_state["Years"][0] == st.session_state["Years"][1]:
+            st.warning("Choose a Month range, not the same month")
+    def accounts() -> None:
+        Accounts= [1,2,3]
+        st.multiselect("Accounts to plot", options=Accounts, key=Accounts)
+    
+
+
+    def init() -> None:
+        csvlogic.readcsv()
+        Dates_UserSelected.yearslider()
+        Dates_UserSelected.monthslider()
+        csvlogic.userselecteddataframe()
+    
+        
+       
+
+
+class Streamlit():
+    def Streamlitinit():
+        st.title("Home Navigation Page")
+        
+
+        Dates_UserSelected.init()
+        pages=Pagedocuments.pages_init()
+        pg = st.navigation(pages, position="top")
+        pg.run()
+class Pagedocuments():
+    def pages_init() -> dict:
+        Path="Pages/"
+        pages = glob.glob(f"{Path}*.py")
+        pagesdict = [ st.Page(page, title=page.removesuffix(".py").removeprefix("Pages/")) for page in pages]
+        return pagesdict
+
+Streamlit.Streamlitinit()
